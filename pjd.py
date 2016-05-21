ea59358faaace72a5cacafa14261d5ce9a809035
FdeFabricio <fabriciocomf@gmail.com>
2016-05-21 11:00:42 -0300

    envia pacotes
    Servidor enviando pacotes para o cliente

--- pjd.py
+++ pjd.py
@@ -1,23 +1,40 @@
 import socket
 
-def activeOpen(SRV,PORT):
-    s = Soquete()
-    return s  
+def activeOpen(SRV,port):
+    soquete = Soquete()
+    host = socket.gethostname()
+    
+    soquete.s.connect((host,port))
+    return soquete
 
-def passiveOpen(PORT):
-    s = Soquete()
-    addr = ""
-    return s, addr
+def passiveOpen(port):
+    soquete = Soquete()
+    host = socket.gethostname()
+    soquete.s.bind((host, port))
+    
+    print "host: ", host
+    print "port: ", port
+    soquete.s.listen(5)
+    
+    conn, addr = soquete.accept()
+    
+    return (conn, addr)
 
 class Soquete:
+    
     def __init__(self):
-        return
+        self.s = socket.socket()
         
     def recv(self, size):
-        return
+        return self.s.recv(size)
     
     def send(self, data):
-        return
+        self.s.send(data)
     
     def close(self):
-        return
+        self.s.close()
+    
+    def accept(self):
+        conn = Soquete()
+        conn.s, addr = self.s.accept()
+        return conn, addr
